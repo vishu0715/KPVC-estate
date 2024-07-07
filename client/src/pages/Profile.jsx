0b616811bb72dd606e615073c54b74639f2fd0ec
vishu0715 <133125417+vishu0715@users.noreply.github.com>
2024-07-07 14:14:12 +0530

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,72 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import {useSelector} from 'react-redux'
-
+import { useRef, useState, useEffect } from 'react';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
+import {app} from '../firebase';
 export default function Profile() {
+  const fileRef = useRef(null);
   const {currentUser} = useSelector(state => state.user);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  },[file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 
+          100;
+        setFilePerc(Math.round(progress));
+      },
+      (error)=>{  
+        setFileUploadError(true);
+      },
+      ()=>{
+        getDownloadURL(uploadTask.snapshot.ref).then
+        ((downloadURL) => 
+          setFormData({...formData, avatar: downloadURL})
+        );
+      }
+    );
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <img src={currentUser.avatar} alt="profile" 
-        className='w-26 h-26 object-cover rounded-full cursor-pointer self-center mt-2'/>
+        <input onChange={(e) => setFile(e.target.files[0])} type='file' hidden ref={fileRef} accept='image/*'/>
+        <img
+  onClick={() => fileRef.current.click()}
+  src={formData.avatar || currentUser.avatar}
+  alt="profile"
+  className='w-24 h-24 object-cover rounded-full cursor-pointer self-center mt-2'
+  style={{ maxWidth: '150px', maxHeight: '150px' }}
+/>
+        <p className='text-sm self-center'>
+          {fileUploadError ?
+            (<span className='text-red-700'>Error uploading Image (image must be less than 2 mb)
+            </span>) :
+          filePerc > 0 && filePerc < 100?(
+             <span className='text-green-700'>{`Uploading ${filePerc}%`}
+             </span>)
+             :
+             filePerc === 100?(
+             <span className='text-green-700'>Image uploaded Successfully</span>)
+             :( ''
+          )}
+        </p>
         <input type="text" placeholder="Username" id='username' className='border p-3 rounded-lg'/>
         <input type="email" placeholder="email" id='email' className='border p-3 rounded-lg'/>
         <input type="password" placeholder="password" id='password' className='border p-3 rounded-lg'/>
