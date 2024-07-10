391ddca6dd4acc705d6e0621548a841b8aafb706
vishu0715 <133125417+vishu0715@users.noreply.github.com>
2024-07-10 11:23:04 +0530

    completed delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -137,6 +137,22 @@ export default function Profile() {
      }
   };
 
+  const handleDeleteListing = async(listingId) => {
+      try{
+          const res = await fetch(`/api/listing/delete/${listingId}`, {
+              method: 'DELETE',
+          });
+          const data = await res.json();
+          if(data.success === false){
+              console.log(data.message);
+              return;
+          }
+          setUserListings(prev => prev.filter((listing) => listing._id !== listingId));
+      }
+      catch(error){
+          console.log(error);
+      }
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -189,7 +205,7 @@ export default function Profile() {
               <p>{listing.name}</p>
             </Link>
             <div className='flex flex-col items-center'>
-                 <button className='text-red-700 uppercase'>Delete</button> 
+                 <button onClick={() => handleDeleteListing(listing._id)} className='text-red-700 uppercase'>Delete</button> 
                  <button className='text-green-700 uppercase'>Edit</button> 
             </div>
         </div>
