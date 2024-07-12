af1fa95ed3f7d86f2a6683d287b93564fedf7b18
vishu0715 <133125417+vishu0715@users.noreply.github.com>
2024-07-12 12:07:49 +0530

    create Search Page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
+import Search from './pages/Search';
 export default function App() {
   return <BrowserRouter>
     <Header />
@@ -19,7 +20,7 @@ export default function App() {
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/about" element={<About />} />
       <Route path="/listing/:listingId" element={<Listing />} />
-
+      <Route path="/search" element={<Search />} />
       <Route element={<PrivateRoute />}>
       <Route path="/profile" element={<Profile />} />
       <Route path="/create-listing" element={<CreateListing />} />
