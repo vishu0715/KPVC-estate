c330062d433e94b6d15346cedeaaad327cabdf1e
vishu0715 <133125417+vishu0715@users.noreply.github.com>
2024-07-12 14:21:01 +0530

    completed show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -15,6 +15,7 @@ export default function Search() {
   });
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
+  const [showMore, setShowMore] = useState(false);
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const searchTermFromUrl = urlParams.get('searchTerm');
@@ -38,9 +39,16 @@ export default function Search() {
     }
     const fetchListings = async () => {
           setLoading(true);
+          setShowMore(false);
           const searchQuery= urlParams.toString();
           const res = await fetch(`/api/listing/get?${searchQuery}`);
           const data = await res.json();
+          if(data.length>8){
+            setShowMore(true);
+          }
+          else{
+            setShowMore(false);
+          }
           setListings(data);
           setLoading(false);
 
@@ -79,6 +87,20 @@ export default function Search() {
     urlParams.set('order',sidebardata.order)
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
+  };
+
+  const onShowMoreClick = async() => {
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        if(data.length<9){
+          setShowMore(false);
+        }
+        setListings([...listings, ...data]);
   };
   return (
     <div className='flex flex-col md:flex-row'>
@@ -149,9 +171,13 @@ export default function Search() {
                 {
                     !loading && listings && listings.map((listing)=>(
                          <ListingItem key={listing._id} listing={listing}/>
-                    ))
+                    ))}
 
-                }
+                    {showMore &&(
+                        <button onClick={onShowMoreClick}className='text-green-700 hover:underline p-7 text-center w-full'>
+                          Show More
+                        </button>
+                    )}
             </div>
         </div>
     </div>
